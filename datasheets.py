b981d95c8941fd2665c843b5157f061acef5d268
Luca Smith <luca.splat.2020@gmail.com>
2026-01-22 15:32:46 +0000

    Add author and license information to datasheets.py
    Added licensing information and author details to the datasheets.py file.

--- datasheets.py
+++ datasheets.py
@@ -1,3 +1,19 @@
+#Written by Bawolfunit
+# This program is designed to help run warhammer games by using digital features. Will try to build a device built around this program. Datasheets must be provided by user
+#Copyright (C) 2026  Luca Smith(Badwolfunit)
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/
+
 import json
 
 from warhammer_gadget.datasheets_data import CONFIG_DIR
