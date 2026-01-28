1092bea36d913ea2b41c438e7e4d41df435d6376
Luca Smith <luca.splat.2020@gmail.com>
2026-01-28 12:37:07 +0000

    Fix formatting of comments in __init__.py

--- warhammer_gadget/__init__.py
+++ warhammer_gadget/__init__.py
@@ -1,20 +1,20 @@
-#Written by Badwolfunit
+# Written by Badwolfunit
 # This program is designed to help run warhammer games by using digital features. Will try to build a device built around this program. Datasheets must be provided by user
-#Copyright (C) 2026  Luca Smith(Badwolfunit)
-#Disclaimer:
-#Warhammer is a trademark of Games Workshop Ltd. This project is not affiliated with, endorsed by, or associated with Games Workshop in any way.
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# Copyright (C) 2026  Luca Smith(Badwolfunit)
+# Disclaimer:
+# Warhammer is a trademark of Games Workshop Ltd. This project is not affiliated with, endorsed by, or associated with Games Workshop in any way.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <https://www.gnu.org/licenses/
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/
 
 from pathlib import Path
 
